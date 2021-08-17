6190d098df010d310379745a56c01d00f8c2a516
Alan Allard <alan.allard@eyevinn.se>
2021-08-17 18:28:14 +0200

    Initial Commit

---
+++ VideoToolboxMacOSExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VideoToolboxMacOSExample
+//
+//  Created by Eyevinn on 2021-08-17.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ VideoToolboxMacOSExample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  VideoToolboxMacOSExample
+//
+//  Created by Eyevinn on 2021-08-17.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
